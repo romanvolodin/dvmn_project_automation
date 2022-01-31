da5d26053aff7fcba1a20773cb80066753a44f02
Oleg Tarasov <tolegu@ya.ru>
2022-01-31 11:49:22 +0300

    создание workspace в trello api

--- core/trello.py
+++ core/trello.py
@@ -19,6 +19,24 @@ def is_trello_board_name(key: str = "", token: str = "", board_name: str = "") -
     return False
 
 
+def is_trello_workspace_name(key: str = "", token: str = "", workspace_name: str = "") -> bool:
+    url = "https://api.trello.com/1/search"
+    params = {
+        "key": key,
+        "token": token,
+        "query": workspace_name,
+        "modelTypes": "organizations",
+        "organization_fields": "displayName",
+        "organizations_limit": 1,
+    }
+    headers = {"Accept": "application/json"}
+    response = requests.request("GET", url, headers=headers, params=params)
+    response.raise_for_status()
+    if len(response.json()["organizations"]) > 0:
+        return True
+    return False
+
+
 def delete_trello_board(key: str = "", token: str = "", board_id: str = "") -> bool:
     url = f"https://api.trello.com/1/boards/{board_id}"
     params = {
@@ -60,6 +78,25 @@ def add_member_to_trello_board(
     return True
 
 
+def create_trello_workspace(
+    key: str = "",
+    token: str = "",
+    display_name: str = "",
+    payload: dict = {}
+    ) -> str:
+    url = "https://api.trello.com/1/organizations"
+    params = {
+        "key": key,
+        "token": token,
+        "displayName": display_name,
+        **payload,
+    }
+    response = requests.request("POST", url, params=params)
+    response.raise_for_status()
+    organization_id = response.json()["id"]
+    return organization_id
+
+
 if __name__ == "__main__":
     from environs import Env
 
@@ -69,25 +106,31 @@ if __name__ == "__main__":
     trello_key = env.str("TRELLO_KEY")
     trello_token = env.str("TRELLO_TOKEN")
 
-    payload = {
+    workspace_payload = {
+        "desc": "Девман. Командный проект Автоматизация-25.01.22",
+    }
+
+    trello_workspace_id = create_trello_workspace(
+            key=trello_key,
+            token=trello_token,
+            display_name="Проект Автоматизация-25.01.22",
+            payload=workspace_payload,
+        )
+    print(f'Создан workspace с ID={trello_workspace_id}')
+
+    board_payload = {
         "name": "TestBoardMember",
         "prefs_background": "orange",
         "desc": "Доска для команды №",
+        "idOrganization": trello_workspace_id,
     }
 
-    is_board = is_trello_board_name(
-        key=trello_key, token=trello_token, board_name=payload["name"]
+    trello_board_id = create_trello_board(
+        key=trello_key,
+        token=trello_token,
+        payload=board_payload,
     )
-
-    if not is_board:
-        trello_board_id = create_trello_board(
-            key=trello_key,
-            token=trello_token,
-            payload=payload,
-        )
-        print(f"Создана доска с ID={trello_board_id}")
-    else:
-        print("Доска с таким названием уже существует.")
+    print(f"Создана доска с ID={trello_board_id}")
 
     add_member_to_trello_board(
         key=trello_key,
